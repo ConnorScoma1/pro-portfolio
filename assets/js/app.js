513eda781ef3f67bfd157aeef6c0044f9f7d9f68
connor scoma <connorscoma@connors-MacBook-Pro.local>
2019-05-05 10:22:59 -0500

    adding scroll animations

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,6 +12,33 @@
 
 //Document Ready Function 
 $(document).ready(function(){
+
+    var $animation_elements = $('.animation-element');
+    var $window = $(window);
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+ 
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+ 
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+      $element.addClass('in-view');
+    } else {
+      $element.removeClass('in-view');
+    }
+  });
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
     //About Button Function
     $('#btn-1').click(function () { 
         //Setting Animation for About
@@ -27,7 +54,7 @@ $(document).ready(function(){
         },500);
 
 
-})
+});
 
 // Resume Animation
 $('#btn-3').click(function () { 
@@ -59,7 +86,4 @@ $('#btn-5').click(function () {
 })
 
 
-
-
 });
-
